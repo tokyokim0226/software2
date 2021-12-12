d83f419eed906bcbf060ebeae567d87f3da717f6
takata-atsuki <takata-atsuki551@g.ecc.u-tokyo.ac.jp>
2021-12-12 22:42:07 +0900

    upd:アドレスを表示

--- physics/struct_alignment.c
+++ physics/struct_alignment.c
@@ -40,13 +40,56 @@ int main (int argc, char**argv)
   /* 以下に各構造体の先頭アドレスを表示するプログラムを書く */
   /* Hint: printf で %p フォーマットを指定する*/
   /* 逆順に表示（s_end, s3, s2, s1, s_begin の順）*/
+  printf("s_end:%p\n",&s_end);
+  printf("s3:%p\n",&s3);
+  printf("s2:%p\n",&s2);
+  printf("s1:%p\n",&s1);
+  printf("s_begin:%p\n",&s_begin);
 
   // do_something!!
 
   /* 以下には各構造体のサイズをsizeof演算子で計算し、表示する */
   /* printf 表示には%zu を用いる*/
+    printf("s_end:%zu\n",sizeof(s_end));
+    printf("s3:%zu\n",sizeof(s3));
+    printf("s2:%zu\n",sizeof(s2));
+    printf("s1:%zu\n",sizeof(s1));
+    printf("s_begin:%zu\n",sizeof(s_begin));
 
   // do_something!!
+
+  //構造体のメンバ変数に割り当てられているアドレスを表示
+  printf("s_end.tag:%p\n",&(s_end.tag));
+  printf("s_end.id:%p\n",&(s_end.id));
+  printf("s_end.name:%p\n",s_end.name);
+  printf("s_end.age:%p\n",&(s_end.age));
+  printf("s_end.height:%p\n",&(s_end.height));
+  printf("s_end.weight:%p\n",&(s_end.weight));
+  printf("s3.tag:%p\n",&(s3.tag));
+  printf("s3.id:%p\n",&(s3.id));
+  printf("s3.name:%p\n",s3.name);
+  printf("s3.age:%p\n",&(s3.age));
+  printf("s3.height:%p\n",&(s3.height));
+  printf("s3.weight:%p\n",&(s3.weight));
+  printf("s2.id:%p\n",&(s2.id));
+  printf("s2.name:%p\n",s2.name);
+  printf("s2.age:%p\n",&(s2.age));
+  printf("s2.height:%p\n",&(s2.height));
+  printf("s2.weight:%p\n",&(s2.weight));
+  printf("s2.tag:%p\n",&(s2.tag));
+  printf("s1.id:%p\n",&(s1.id));
+  printf("s1.name:%p\n",s1.name);
+  printf("s1.age:%p\n",&(s1.age));
+  printf("s1.height:%p\n",&(s1.height));
+  printf("s1.weight:%p\n",&(s1.weight));
+  printf("s_begin.id:%p\n",&(s_begin.id));
+  printf("s_begin.name:%p\n",s_begin.name);
+  printf("s_begin.age:%p\n",&(s_begin.age));
+  printf("s_begin.height:%p\n",&(s_begin.height));
+  printf("s_begin.weight:%p\n",&(s_begin.weight));
+
+
+
   
   return 0;
 }
