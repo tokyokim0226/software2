8cf41b4dc0c3b6aa1462d22336f96598e8a2b462
takata-atsuki <takata-atsuki551@g.ecc.u-tokyo.ac.jp>
2021-12-24 13:12:27 +0900

    feat:change_color

--- paint/paint4.c
+++ paint/paint4.c
@@ -10,12 +10,12 @@ typedef struct
     int width;
     int height;
     char **canvas;
+    int **color;
+    int current_color;
     char pen;
-    int color;
+    int back_color;
 } Canvas;
 
-// Command 構造体と History構造体
-// [*]
 typedef struct command Command;
 struct command{
     char *str;
@@ -26,11 +26,11 @@ struct command{
 typedef struct
 {
     Command *begin;
-    size_t bufsize; // [*] : この方が効率的ですね。一部の方から指摘ありました。
+    size_t bufsize; 
 } History;
 
 // functions for Canvas type
-Canvas *init_canvas(int width, int height, char pen, int color);
+Canvas *init_canvas(int width, int height, char pen, int color, int back_color);
 void reset_canvas(Canvas *c);
 void print_canvas(Canvas *c);
 void free_canvas(Canvas *c);
@@ -41,7 +41,7 @@ void clear_command(void);
 void clear_screen(void);
 
 // enum for interpret_command results
-typedef enum res{ EXIT, LINE, UNDO, SAVE, UNKNOWN, ERRNONINT, ERRLACKARGS, NOCOMMAND, CHPEN, COLORNUMERROR, CHBACKGROUNDCOLOR} Result;
+typedef enum res{ EXIT, LINE, UNDO, SAVE, UNKNOWN, ERRNONINT, ERRLACKARGS, NOCOMMAND, CHPEN, COLORNUMERROR, CHBACKGROUNDCOLOR, CHCOLOR} Result;
 // Result 型に応じて出力するメッセージを返す
 char *strresult(Result res);
 
@@ -50,9 +50,9 @@ void draw_line(Canvas *c, const int x0, const int y0, const int x1, const int y1
 Result interpret_command(const char *command, History *his, Canvas *c);
 void save_history(const char *filename, History *his);
 void change_pen(Canvas *c,char pen);
-void change_background_color(Canvas *c,int color);
+void change_color(Canvas *c,int color);
+void change_background_color(Canvas *c,int back_color);
 
-// [*] list.c のpush_backと同じ
 Command *push_command(History *his, const char *str);
 
 int main(int argc, char **argv)
@@ -61,7 +61,6 @@ int main(int argc, char **argv)
     
     const int bufsize = 1000;
     
-    // [*]
     History his = (History){ .begin = NULL, .bufsize = bufsize};
     
     int width;
@@ -85,18 +84,16 @@ int main(int argc, char **argv)
 	height = (int)h;    
     }
     char pen = '*';
-    int color = 40;
+    int back_color = 40;
+    int color = 37;
 
     char buf[bufsize];
 
-    Canvas *c = init_canvas(width,height, pen, color);
+    Canvas *c = init_canvas(width,height, pen, color, back_color);
     
     printf("\n"); // required especially for windows env
 
     while(1){
-	// [*]
-	// hsize はひとまずなし
-	// 作る場合はリスト長を調べる関数を作っておく
 	print_canvas(c);
 	printf("* > ");
 	if(fgets(buf, bufsize, stdin) == NULL) break;
@@ -109,8 +106,7 @@ int main(int argc, char **argv)
 	clear_command();
 	printf("%s\n",strresult(r));
 	// LINEの場合はHistory構造体に入れる
-	if ((r == LINE) || (r == CHPEN)) {
-	    // [*]
+	if ((r == LINE) || (r == CHPEN) || (r == CHBACKGROUNDCOLOR) || (r == CHCOLOR)) {
 	    push_command(&his,buf);
 	}
 	
@@ -126,21 +122,28 @@ int main(int argc, char **argv)
     return 0;
 }
 
-Canvas *init_canvas(int width,int height, char pen, int color)
+Canvas *init_canvas(int width,int height, char pen, int color, int back_color)
 {
     Canvas *new = (Canvas *)malloc(sizeof(Canvas));
     new->width = width;
     new->height = height;
     new->canvas = (char **)malloc(width * sizeof(char *));
+    new->color = (int **)malloc(width * sizeof(int *));
     
     char *tmp = (char *)malloc(width*height*sizeof(char));
+    int *ctmp = (int *)malloc(width*height*sizeof(int));
     memset(tmp, ' ', width*height*sizeof(char));
+    for(int i = 0; i < width*height; i++){
+        ctmp[i] = color;
+    }
     for (int i = 0 ; i < width ; i++){
 	new->canvas[i] = tmp + i * height;
+    new->color[i] =  ctmp + i * height;
     }
 
-    new -> color = color;
+    new -> back_color = back_color;
     new->pen = pen;
+    new->current_color = color;
     return new;
 
 }
@@ -150,8 +153,14 @@ void reset_canvas(Canvas *c)
     const int width = c->width;
     const int height = c->height;
     c -> pen= '*';
-    c -> color = 40;
+    c -> back_color = 40;
+    c -> current_color = 37;
     memset(c->canvas[0], ' ', width*height*sizeof(char));
+    for(int y = 0; y < height; y++){
+        for(int x = 0; x < width; x++){
+            c->color[y][x] = c->current_color;
+        }
+    }
 }
 
 
@@ -160,6 +169,7 @@ void print_canvas(Canvas *c)
     const int height = c->height;
     const int width = c->width;
     char **canvas = c->canvas;
+    int **color = c->color; 
     
     // 上の壁
     printf("+");
@@ -172,8 +182,11 @@ void print_canvas(Canvas *c)
 	printf("|");
 	for (int x = 0 ; x < width; x++){
 	    const char a = canvas[x][y];
+        const int b = color[x][y];
 	    //putchar(c);
-        printf("\e[%dm%c\e[0m",c -> color,a);
+        printf("\e[%dm",c -> back_color);
+        printf("\e[%dm%c\e[0m",b,a);
+        printf("\e[0m");
 	}
 	printf("|\n");
     }
@@ -188,6 +201,8 @@ void print_canvas(Canvas *c)
 
 void free_canvas(Canvas *c)
 {
+    free(c->color[0]); //  for 2-D array free
+    free(c->color);
     free(c->canvas[0]); //  for 2-D array free
     free(c->canvas);
     free(c);
@@ -217,8 +232,11 @@ int max(const int a, const int b)
 void change_pen(Canvas *c,char pen){
     c -> pen = pen;
 }
-void change_background_color(Canvas *c,int color){
-    c -> color = color;
+void change_background_color(Canvas *c,int back_color){
+    c -> back_color = back_color;
+}
+void change_color(Canvas *c,int color){
+    c->current_color = color;
 }
 void draw_line(Canvas *c, const int x0, const int y0, const int x1, const int y1)
 {
@@ -234,6 +252,7 @@ void draw_line(Canvas *c, const int x0, const int y0, const int x1, const int y1
 	const int y = y0 + i * (y1 - y0) / n;
 	if ( (x >= 0) && (x< width) && (y >= 0) && (y < height))
 	    c->canvas[x][y] = pen;
+        c->color[x][y] = c->current_color;
     }
 }
 
@@ -248,7 +267,6 @@ void save_history(const char *filename, History *his)
 	fprintf(stderr, "error: cannot open %s.\n", filename);
 	return;
     }
-    // [*] 線形リスト版
     for (Command *p = his->begin ; p != NULL ; p = p->next){
 	fprintf(fp, "%s", p->str);
     }
@@ -266,7 +284,7 @@ Result interpret_command(const char *command, History *his, Canvas *c)
     if (s == NULL){ // 改行だけ入力された場合
 	return UNKNOWN;
     }
-    if (strcmp(s, "chback") == 0) {
+    if (strcmp(s, "chcolor") == 0) {
 	s = strtok(NULL, " ");
     if(s == NULL){
         return ERRLACKARGS;
@@ -277,10 +295,27 @@ Result interpret_command(const char *command, History *his, Canvas *c)
 	    return ERRNONINT;
     }
     int color = (int)w;
-    if(!((color >= 40) && (color <= 47))){
+    if(!((color >= 30) && (color <= 37))){
         return COLORNUMERROR;
     }
-	change_background_color(c,color);
+	change_color(c,color);
+	return CHCOLOR;
+    }
+    if (strcmp(s, "chback") == 0) {
+	s = strtok(NULL, " ");
+    if(s == NULL){
+        return ERRLACKARGS;
+    }
+    char *e;
+    long w = strtol(s,&e,10);
+    if (*e != '\0'){
+	    return ERRNONINT;
+    }
+    int back_color = (int)w;
+    if(!((back_color >= 40) && (back_color <= 47))){
+        return COLORNUMERROR;
+    }
+	change_background_color(c,back_color);
 	return CHBACKGROUNDCOLOR;
     }
     if (strcmp(s, "chpen") == 0) {
@@ -323,8 +358,6 @@ Result interpret_command(const char *command, History *his, Canvas *c)
     
     if (strcmp(s, "undo") == 0) {
 	reset_canvas(c);
-	//[*] 線形リストの先頭からスキャンして逐次実行
-	// pop_back のスキャン中にinterpret_command を絡めた感じ
 	Command *p = his->begin;
 	if (p == NULL){
 	    return NOCOMMAND;
@@ -356,7 +389,6 @@ Result interpret_command(const char *command, History *his, Canvas *c)
 }
 
 
-// [*] 線形リストの末尾にpush する
 Command *push_command(History *his, const char *str){
     Command *c = (Command*)malloc(sizeof(Command));
     char *s = (char*)malloc(his->bufsize);
@@ -402,6 +434,8 @@ char *strresult(Result res){
     return "the number of color is not existed";
     case CHBACKGROUNDCOLOR:
     return "changed backgroundcolor";
+    case CHCOLOR:
+    return "changed pen color";
     }
     return NULL;
 }
